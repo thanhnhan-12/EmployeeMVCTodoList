0e5327997d719244e0054f4d73e0a5def273df01
thanhnhan-12 <nguyenthanhnhan12701@gmail.com>
2024-01-06 15:53:05 +0700

    Create Database

---
+++ EmployeeMVCTodoList/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeMVCTodoList.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMVCTodoList.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
